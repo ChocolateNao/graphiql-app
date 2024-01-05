0fc589c57d3f1532a144fcefd15076194780aa28
ChocolateNao <andrey12q112@gmail.com>
2024-01-05 04:43:15 +0300

    feat: add test, translations, moved imgs to work with deploy

--- src/pages/EndpointSettings/EndpointSettings.tsx
+++ src/pages/EndpointSettings/EndpointSettings.tsx
@@ -1,19 +1,22 @@
 import { NavLink } from 'react-router-dom';
 
+import { useLocalization } from 'shared/context/LocalizationContext';
+
 import styles from './EndpointSettings.module.scss';
 
 function EndpointSettings() {
+  const { t } = useLocalization();
   return (
     <div className={styles.wrapper}>
       <div className={styles.window}>
         <div className={styles.window_header}>
-          <div className={styles.title}>Endpoint Settings</div>
+          <h1 className={styles.title}>
+            {t('endpointSettings.endpointSettingsHeading')}
+          </h1>
           <NavLink className={styles.close_btn} to="/home" />
         </div>
         <div className={styles.window_main}>
-          <div className={styles.title}>
-            Use built-in proxy in case of CORS errors
-          </div>
+          <div className={styles.title}>{t('endpointSettings.useProxy')}</div>
           <div className={styles.btn_switch}>
             <label className={styles.switch}>
               <input type="checkbox" />
