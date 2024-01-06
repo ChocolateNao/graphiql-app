8d5ad86eec0961577057ffcdd7e89d41fb306070
ChocolateNao <andrey12q112@gmail.com>
2024-01-06 04:47:12 +0300

    refactor: move localization conf into a separate file

--- src/models/LocalizationConfig.interface.ts
+++ src/models/LocalizationConfig.interface.ts
@@ -1,6 +1,10 @@
-import { SupportedLocale } from 'utils/localizationConfig';
-
 export interface LocalizationConfig {
   defaultLocale: SupportedLocale;
   fallbackLocale: SupportedLocale;
 }
+
+export type Translation = {
+  [key: string]: string;
+};
+
+export type SupportedLocale = 'en_US' | 'ru_RU';
